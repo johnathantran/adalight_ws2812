a450d3ceafcb5e79de9821c44298cb76d9f39076
Johnathan Tran <jtran304@gmail.com>
2020-05-22 17:17:17 -0500

    Ambilight LED strips

--- Adalight_WS2812.ino
+++ Adalight_WS2812.ino
@@ -7,8 +7,8 @@
  * @date: 11/22/2015
  */
 #include "FastLED.h"
-#define NUM_LEDS 240
-#define DATA_PIN 6
+#define NUM_LEDS 60
+#define DATA_PIN 5
 
 // Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
 #define serialRate 115200
